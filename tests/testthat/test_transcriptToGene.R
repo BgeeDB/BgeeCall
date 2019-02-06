5ccd189b81c18f9cb45bc140e34d2a152fec5cf2
Julien Wollbrett <jwollbrett@bio-51-170.unil.ch>
2019-02-06 14:31:25 +0100

    correct tests
    Former-commit-id: a205db7c08431d82ea41fc04fe799d70845c1bae

--- tests/testthat/test_transcriptToGene.R
+++ tests/testthat/test_transcriptToGene.R
@@ -2,17 +2,18 @@ context('testing generation of gene to biotype file')
 library(AnnotationHub)
 
 test_that("test generation of intergenic transcript to gene", {
-  bgee <- new("BgeeMetadata", bgee_release = "14")
+  bgee <- new("BgeeMetadata", intergenic_release = "0.1")
   user <- new("UserMetadata")
   user@species_id <- "6239" # C. elegans
   expect_failure(expect_error(intergenic_txToGene <- intergenic_tx2gene(myBgeeMetadata = bgee, myUserMetadata = user), NULL))
   expect_equal(nrow(intergenic_txToGene), 4277)
   expect_equal(ncol(intergenic_txToGene), 2)
-  unlink(BgeeCall:::get_bgee_release_path(myBgeeMetadata = bgee, myUserMetadata = user), recursive = TRUE)
+  unlink(BgeeCall:::get_intergenic_release_path(myBgeeMetadata = bgee, myUserMetadata = user), recursive = TRUE)
+  unlink(file.path(user@working_path, "release.tsv"))
 })
 
 test_that("test creation of transcript to gene file", {
-  bgee <- new("BgeeMetadata", bgee_release = "14")
+  bgee <- new("BgeeMetadata", intergenic_release = "0.1")
   kallisto <- new("KallistoMetadata")
   user <- new("UserMetadata")
   user@species_id <- "6239" # C. elegans
@@ -25,6 +26,7 @@ test_that("test creation of transcript to gene file", {
   expect_failure(expect_error(txToGene <- read.csv(create_tx2gene(myAbundanceMetadata = kallisto, myBgeeMetadata = bgee, myUserMetadata = user), header = TRUE, sep = "\t"), NULL))
   expect_equal(nrow(txToGene), 62111)
   expect_equal(ncol(txToGene), 2)
-  unlink(BgeeCall:::get_bgee_release_path(myBgeeMetadata = bgee, myUserMetadata = user), recursive = TRUE)
+  unlink(BgeeCall:::get_intergenic_release_path(myBgeeMetadata = bgee, myUserMetadata = user), recursive = TRUE)
+  unlink(file.path(user@working_path, "release.tsv"))
 })
 
