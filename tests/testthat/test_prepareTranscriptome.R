5fa200709e5f2af09e564e0a390e01549680b4d7
Julien Wollbrett <jwollbrett@bio-51-170.unil.ch>
2019-04-15 16:16:52 +0200

    optimize checking time
    Former-commit-id: 144b48df8639dfdf5f28bb6010d7d2da62e84e3b

--- tests/testthat/test_prepareTranscriptome.R
+++ tests/testthat/test_prepareTranscriptome.R
@@ -5,14 +5,10 @@ test_that ("test merging of transcriptome and intergenic regions", {
   kallisto <- new ("KallistoMetadata")
   user <- new("UserMetadata")
   user@species_id <- "6239" # C. elegans
-  ah = AnnotationHub()
-  # query the annotation hub
-  ah_resources <- AnnotationHub::query(ah, c("Ensembl", "Caenorhabditis elegans", "84"))
-  transcriptome_object <- rtracklayer::import.2bit(ah_resources[["AH50453"]])
+  transcriptome_file <- system.file("extdata", "transcriptome.fa", package = "BgeeCall")
   # retrieve dataset locally and keep path to local file
-  user <- setTranscriptomeFromObject(user, transcriptome_object, "WBcel235")
+  user <- setTranscriptomeFromFile(user, transcriptome_file, "WBcel235")
   user@working_path <- getwd()
-  merge_transcriptome_and_intergenic(kallisto, bgee, user)
   # test that no error is produced
   expect_failure(expect_error(merge_transcriptome_and_intergenic(kallisto, bgee, user), NULL))
   unlink(BgeeCall:::get_intergenic_release_path(myBgeeMetadata = bgee, myUserMetadata = user), recursive = TRUE)
