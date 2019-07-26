34617c7a70b753dc236611da671223d922618d2c
Julien Wollbrett <jwollbrett@bio-51-170.unil.ch>
2019-07-26 15:17:59 +0200

    solve bugs and update doc
    Former-commit-id: 5ec52ec02ec8ef089bfdcdc8182b9682674d1e5e

--- R/retrieveFromCommunity.R
+++ R/retrieveFromCommunity.R
@@ -7,48 +7,70 @@
 #' - numberOfLibraries : number of libraries used to generate these reference intergenic
 #' sequences
 #'
+#' @author Julien Wollbrett
+#' 
 #' @import jsonlite
 #' 
+#' @export
+#'
 #' @return list all species having reference intergenic sequences created by the community
 #' 
 #' @examples{
 #' list_community_ref_intergenic_species()
 #' }
 #'
-#' @author Julien Wollbrett
-#' @export
-#'
 #'
 list_community_ref_intergenic_species <- function() {
-    community <- fromJSON(txt = "https://sandbox.zenodo.org/api/records/?communities=test_community2")
+    community <- jsonlite::fromJSON(txt = "https://sandbox.zenodo.org/api/records/?communities=test_community2")
     datasets <- NULL
     datasets_index <- 1
     for (records_index in 1:nrow(community)) {
         record <- community[records_index,]
-       keywords <- unlist(record$metadata$keywords)
-       speciesId <- unlist(strsplit( x= as.character(
+        keywords <- unlist(record$metadata$keywords)
+        species_id <- unlist(strsplit( x= as.character(
            keywords[grep("speciesId", keywords)]), split = ":"))[2]
-       type_intergenic <- unlist(strsplit(x= as.character(
-           keywords[grep("type", keywords)]), split = ":"))[2]
-       number_libraries <- unlist(strsplit(x= as.character(
+        annotation_version <- unlist(strsplit(x= as.character(
+           keywords[grep("annotationVersion", keywords)]), split = ":"))[2]
+        genome_version <- unlist(strsplit(x= as.character(
+            keywords[grep("genomeVersion", keywords)]), split = ":"))[2]
+        number_libraries <- unlist(strsplit(x= as.character(
            keywords[grep("numberOfLibraries", keywords)]), split = ":"))[2]
-       urls <- as.data.frame(record$files)
-       # test presence of all mandatory metadata
-       if (!(is.na(urls) || is.na(speciesId) || is.na(type_intergenic) || 
-             type_intergenic != "ref_intergenic")) {
+        kallisto_version <- unlist(strsplit(x= as.character(
+            keywords[grep("kallistoVersion", keywords)]), split = ":"))[2]
+        urls <- as.data.frame(record$files)
+        # test presence of all mandatory metadata
+        if (!(is.null(urls$links$download) || is.null(species_id))) {
             # mandatory to have at least one file when uploading new dataset
             for (url_index in 1:nrow(urls)) {
                 if(grepl(basename(urls[url_index,]$links$download), 
-                         paste0(speciesId,"_intergenic.fa.gz"))) {
-                    datasets$speciesId[datasets_index] <- as.character(speciesId)
-                    datasets$url[datasets_index] <- as.character(urls[url_index,]$links$download)
+                         paste0(species_id,"_intergenic.fa.gz"))) {
+                    datasets$speciesId[datasets_index] <- as.character(species_id)
                     if(is.null(number_libraries)) {
                         datasets$numberOfLibraries[datasets_index] <- NA
                     } else {
                         datasets$numberOfLibraries[datasets_index] <- 
                             as.character(number_libraries)
-                   }
-                datasets_index <- datasets_index + 1
+                    }
+                    if(is.null(annotation_version)) {
+                        datasets$annotationVersion[datasets_index] <- NA
+                    } else {
+                        datasets$annotationVersion[datasets_index] <- 
+                            as.character(annotation_version)
+                    }
+                    if(is.null(genome_version)) {
+                        datasets$genomeVersion[datasets_index] <- NA
+                    } else {
+                        datasets$genomeVersion[datasets_index] <- 
+                            as.character(genome_version)
+                    }
+                    if(is.null(kallisto_version)) {
+                        datasets$kallistoVersion[datasets_index] <- NA
+                    } else {
+                        datasets$kallistoVersion[datasets_index] <- 
+                            as.character(kallisto_version)
+                    }
+                    datasets$url[datasets_index] <- as.character(urls[url_index,]$links$download)
+                    datasets_index <- datasets_index + 1
                 }
             }
         }
@@ -59,10 +81,11 @@ list_community_ref_intergenic_species <- function() {
 retrieve_community_ref_intergenic_url <- function(speciesId, speciesDataSet = NULL) {
     species_dataset <- speciesDataSet
     if(is.null(species_dataset)) {
-        species_dataset <- retrieve_community_ref_intergenic_species()
+        species_dataset <- list_community_ref_intergenic_species()
     }
     if(! (as.character(speciesId) %in% as.character(species_dataset$speciesId))) {
-        stop("No reference intergenic sequences available for speciesId ", speciesId, ".")
+        stop("No reference intergenic sequences available for speciesId ", 
+             speciesId, " in the community release")
     }
     file <- species_dataset$url[species_dataset$speciesId == as.character(speciesId)]
     return(as.character(file))
