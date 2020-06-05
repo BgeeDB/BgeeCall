2d6a7131b4541d76f42e5a9ee59b28ab9b6f6c6f
Julien <julien.wollbrett@gmail.com>
2020-06-05 15:57:24 +0200

    submit jobs to cluster

--- R/runCallsSlurm.R
+++ R/runCallsSlurm.R
@@ -82,7 +82,7 @@ generate_slurm_indexes <- function(kallistoMetadata = new("KallistoMetadata"),
   }
   
   sjobs <- rslurm::slurm_apply(f = index_wrapper, params = unique_df, jobname = "generate_index", 
-                              nodes = 1, cpus_per_node = 1, submit = FALSE, 
+                              nodes = 1, cpus_per_node = 1, submit = TRUE, 
                               add_objects = c("kallistoMetadata", "bgeeMetadata", "userMetadata"), 
                               sh_template = submit_sh_template, rscript_path = rscript_path)
   return(sjobs)
