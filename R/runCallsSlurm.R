c52543307402399a326d5dbc09929a335f61b386
Julien <julien.wollbrett@gmail.com>
2020-06-10 13:40:19 +0200

    first implementation of calls generation on slurm queuing system

--- R/runCallsSlurm.R
+++ R/runCallsSlurm.R
@@ -90,7 +90,6 @@ generate_slurm_indexes <- function(kallistoMetadata = new("KallistoMetadata"),
                                        myUserMetadata = userMetadata)
     create_kallisto_index(myKallistoMetadata = kallistoMetadata, myBgeeMetadata = bgeeMetadata, 
                           myUserMetadata = userMetadata)
-    message("run index generation for species ",userMetadata@species_id)
   }
   
   sjobs <- rslurm::slurm_apply(f = index_wrapper, params = unique_df, jobname = "generate_index", 
@@ -100,6 +99,97 @@ generate_slurm_indexes <- function(kallistoMetadata = new("KallistoMetadata"),
   return(sjobs)
 }
 
+#' 
+#' @title Generate present/absent calls on slurm queuing system 
+#' 
+#' @description This function is meant to be used with a cluster where the Slurm queuing 
+#' system is installed. It processes all steps to generate present/absent calls at RNA-Seq 
+#' library level. This function does not generate the kallisto indexes. If they are not 
+#' already generated please run function ```generate_slurm_indexes``` first.
+#' Steps of present/absent gene expression calls generation are :
+#' \itemize{
+#'   \item Quantifying abundances of transcripts from RNA-Seq libraries
+#'   \item Summarizing abundance at gene level 
+#'   \item generate present/absent expression calls
+#' } 
+#' 
+#' @param abundanceMetadata A Reference Class BgeeMetadata object (optional)
+#' allowing to tune your gene quantification abundance analyze. If no object is
+#' provided a new one will be created with default values.
+#' @param bgeeMetadata A Reference Class BgeeMetadata object (optional)
+#' allowing to choose the version of reference intergenic sequences. If no object
+#' is provided a new one will be created with default values.
+#' @param userFile Path to the file where each line corresponds to one abundance
+#' quantification to be run. The structure of the file is the same than the 
+#' `userFile` used as input of  the `generate_calls_workflow` function.
+#' A template of this file can be loaded with the command :
+#' ```inputFile <- read.table(system.file("userMetadataTemplate.tsv", package = "BgeeCall"), 
+#'                 header = TRUE)```
+#' It is important to keep the same column names.
+#' @param submit_sh_template A template of the bash script used to submit the jobs.
+#' By default the submition script provided by rslurm is used. Modify only if module 
+#' dependancies have to be added  (like kallisto or R)
+#' @param modules A list of modules you want to load in the invironment. Should stay
+#' empty except if you need to load R and/or kallisto (e.g module add R)
+#' @param slurm_options A named list of options recognized by sbatch. More details in
+#' the documentation of the rslurm::slurm_apply function
+#' @param rscript_path The location of the Rscript command. If not specified, defaults 
+#' to the location of Rscript within the R installation being run.
+#' @param submit Whether or not to submit the job to the cluster with sbatch. Default 
+#' value is TRUE
+#' @param nodes The (maximum) number of cluster nodes to spread the calculation over. 
+#' slurm_apply automatically divides params in chunks of approximately equal size to 
+#' send to each node. Less nodes are allocated if the parameter set is too small to 
+#' use all CPUs on the requested nodes. By default this number is 10.
+
+
+#' 
+#' @return generate calls
+#' 
+#' @export
+#' @import rslurm
+#' 
+#' @examples 
+#' \dontrun{
+#' # use function with all default values
+#' userFile <- "/path/to/userList.tsv"
+#' sjobs <- generate_slurm_calls(userFile = userFile)
+#' }
+
+generate_slurm_calls <- function(kallistoMetadata = new("KallistoMetadata"), 
+                                   bgeeMetadata = new("BgeeMetadata"), 
+                                   userMetadata = new("UserMetadata"), userFile, 
+                                   submit_sh_template = NULL, slurm_options = NULL,
+                                   rscript_path = NULL, modules = NULL, submit = TRUE, 
+                                   nodes = 10) {
+  user_df <- read.table(file = userFile, header = TRUE, sep = "\t")
+
+  #use both rscript_path and modules variables to tune the bash template script
+  rscript_path <- generateRScriptPath(rscript_path, modules)
+  
+  #define function used to generate present/absent calls. This function has to be defined inside of the function
+  # calling slurm_apply (enclosing environment (http://adv-r.had.co.nz/Environments.html#function-envs))
+  calls_wrapper <- function(species_id, run_ids, reads_size, rnaseq_lib_path, transcriptome_path, 
+                            annotation_path, output_directory) {
+    userMetadata@species_id <- as.character(species_id) 
+    userMetadata@run_ids = as.character(run_ids)
+    userMetadata@reads_size = as.integer(reads_size)
+    userMetadata@rnaseq_lib_path = as.character(rnaseq_lib_path)
+    outputDir <- as.character(output_directory)
+    if (length(outputDir) != 0) {
+      userMetadata <- setOutputDir(userMetadata, outputDir)
+    }
+    userMetadata <- setTranscriptomeFromFile(userMetadata, transcriptomePath = as.character(transcriptome_path))
+    generate_presence_absence(myKallistoMetadata = kallistoMetadata, myBgeeMetadata = bgeeMetadata, 
+                          myUserMetadata = userMetadata)
+  }
+  
+  sjobs <- rslurm::slurm_apply(f = calls_wrapper, params = user_df, jobname = "generate_calls", 
+                               nodes = nodes, cpus_per_node = 1, submit = submit, 
+                               add_objects = c("kallistoMetadata", "bgeeMetadata", "userMetadata"), 
+                               sh_template = submit_sh_template, rscript_path = rscript_path, slurm_options = slurm_options)
+  return(sjobs)
+}
 # internal function hacking the rscript_path variable of rslurm to automatically add
 # module dependancies in the bash script generated by rslurm
 generateRScriptPath <- function(rscript_path, modules) {
