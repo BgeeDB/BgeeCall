921ce2bb29c46f7458791616a7b957feea83ae45
Julien Wollbrett <julien.wollbrett@unil.ch>
2026-07-03 10:23:43 +0200

    solve bug on tx_import example

--- R/imports.R
+++ R/imports.R
@@ -3,7 +3,7 @@
 #' @importFrom data.table data.table fread fwrite rbindlist setDT setkey setnames
 #' @importFrom Biostrings DNAString DNAStringSet readDNAStringSet writeXStringSet width subseq reverseComplement
 #' @importFrom IRanges IRanges coverage slice
-#' @importFrom txdbmaker makeTxDbFromGFF
+#' @importFrom txdbmaker makeTxDbFromGFF makeTxDbFromGRanges
 #' @importFrom GenomicFeatures exonsBy transcriptsBy
 #' @importFrom rtracklayer import export
 #' @importFrom readr parse_date read_tsv write_tsv
