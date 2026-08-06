7cb7cabd70f37243293cbe452e6403609a6b9177
Alessandro Brandulas Cammarata <alessandro.brandulas.cammarata@gmail.com>
2026-08-06 15:56:02 +0200

    Declared missing packages used in imports field and in imports.R, updated Namespace. Added Matrix for single-cell sparse matrix handling

--- R/imports.R
+++ R/imports.R
@@ -1,20 +1,28 @@
-#' @importFrom stats density p.adjust pnorm qnorm sd median
-#' @importFrom dplyr %>% arrange bind_rows distinct mutate n pull rename summarise ungroup
-#' @importFrom data.table data.table fread fwrite rbindlist setDT setkey setnames
-#' @importFrom Biostrings DNAString DNAStringSet readDNAStringSet writeXStringSet width subseq reverseComplement
-#' @importFrom IRanges IRanges coverage slice
+#' @importFrom stats aggregate approxfun complete.cases density integrate
+#' @importFrom stats median na.omit p.adjust pnorm qnorm quantile sd
+#' @importFrom stats weighted.mean
+#' @importFrom dplyr %>% arrange bind_rows distinct mutate n pull rename
+#' @importFrom dplyr summarise ungroup
+#' @importFrom data.table data.table fread fwrite rbindlist setDT setkey
+#' @importFrom data.table setnames
+#' @importFrom Biostrings DNAString DNAStringSet readDNAStringSet
+#' @importFrom Biostrings reverseComplement subseq width writeXStringSet
+#' @importFrom IRanges IRanges coverage end slice
 #' @importFrom GenomicFeatures exonsBy transcriptsBy
 #' @importFrom txdbmaker makeTxDbFromGRanges
 #' @importFrom rtracklayer import export
 #' @importFrom biomaRt useEnsembl getBM
-#' @importFrom readr parse_date read_tsv write_tsv
+#' @importFrom readr cols parse_date read_tsv write_tsv
 #' @importFrom sjmisc str_contains
 #' @importFrom tximport tximport summarizeToGene
 #' @importFrom methods new slot "slot<-" is
-#' @importFrom ggplot2 ggplot aes geom_density geom_vline scale_color_manual labs theme_minimal scale_fill_manual theme element_blank element_text element_rect
+#' @importFrom ggplot2 aes element_blank element_rect element_text
+#' @importFrom ggplot2 geom_density geom_vline ggplot labs scale_color_manual
+#' @importFrom ggplot2 scale_fill_manual theme theme_minimal
 #' @importFrom grDevices dev.off pdf
-#' @importFrom graphics legend lines par plot
-#' @importFrom utils read.table write.table
+#' @importFrom graphics arrows axis legend lines mtext par plot
+#' @importFrom utils download.file head packageVersion read.table untar
+#' @importFrom utils unzip write.table
 #' @importFrom rslurm slurm_apply get_job_status get_slurm_out
 #' @importFrom RCurl getURL
 #' @importFrom stringr str_extract str_detect str_match
